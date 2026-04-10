e04d7efcfa21ebb4cfc4a0b3bfcf07c84ea81f3c
Bhavesh Joshi <bjwrkstuff@gmail.com>
2026-04-10 12:23:59 +0530

    React basics

---
+++ bj/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import Greeting from './Greeting'
+
+function App() {
+  
+
+  return (
+    <>
+      <Greeting/>
+    </>
+  )
+}
+
+export default App
