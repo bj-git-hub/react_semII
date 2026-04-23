a990e924e2d246c4a300129967c8e3483e7dfca0
Bhavesh Joshi <bjwrkstuff@gmail.com>
2026-04-23 23:56:03 +0530

    misc lectures push

---
+++ app/router/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import NavBar from './components/NavBar';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import NotFound from './pages/NotFound';
+import Home from './pages/Home';
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <Router>
+     <NavBar/>
+
+      <Routes>
+        <Route path = "/" element = {<Home/>} />
+        <Route path ="/about" element = {<About />} />
+        <Route path = "/contact" element = {<Contact />} />
+
+        <Route path = "*" element={<NotFound/>}/>
+      </Routes>
+    </Router>
+    </>
+  )
+}
+
+export default App
